8738dbdac918cbd659823dd6a2628d83f804c0f1
Jefferson Cordero <jepjephooray05@gmail.com>
2014-10-16 00:43:34 +0800

    Finished:
    1. spawn tetrimino
    2. tetrimino is falling down, falling down
    3. keyboard functions - left right down

--- src/gamedev/tetris/GameFrame.java
+++ src/gamedev/tetris/GameFrame.java
@@ -1,8 +1,18 @@
 package gamedev.tetris;
 
+import gamedev.piece.JPiece;
+import gamedev.piece.LPiece;
+import gamedev.piece.LinePiece;
+import gamedev.piece.SPiece;
+import gamedev.piece.SquarePiece;
+import gamedev.piece.ZPiece;
+
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import com.golden.gamedev.Game;
@@ -10,13 +20,15 @@ import com.golden.gamedev.Game;
 public class GameFrame extends Game{
 
 	int width = 10, height = 20, 
-			size = 25, boardLocX, boardLocY;
+			size = 25, boardLocX, boardLocY,
+			numOfPieces = 6, timer = 0, speed = 0;
 	
 	List<Tetrimino> savedPieces, // the pieces that were saved by the user
 	nextPieces,  // the pieces that are next on the list
+	
 	availablePieces; // all the types of tetriminos
 	Tetrimino currentPiece; // current piece falling 
-	boolean spawn = !false;
+	boolean spawn = !false, move = true;
  	
 	long fallTime, fallDelay = 500;
 	
@@ -25,6 +37,7 @@ public class GameFrame extends Game{
 
 	@Override
 	public void initResources() {
+		availablePieces = new ArrayList<Tetrimino>();
 		boardLocX = (getWidth() / 2) - (width*size)/2;
 		boardLocY = (getHeight() / 2) - (height*size)/2;
 	
@@ -33,7 +46,9 @@ public class GameFrame extends Game{
 	
 	private void initGameState() {
 		fallTime = 0;
-		fallDelay = 500;
+		fallDelay = 900;
+		initializePieces();
+		
 	}
 
 	@Override
@@ -42,14 +57,23 @@ public class GameFrame extends Game{
 		gd.fillRect(0, 0, getWidth(), getHeight());
 		
 		gd.setColor(Color.white);
+		gd.drawImage(getImage("img/board.png"), boardLocX - 15, boardLocY - 15, null);
 		for (int i = 0; i < width; i++) {
 			for (int j = 0; j < height; j++) {
 				gd.drawImage(getImage("img/empty.png"), i*size + boardLocX, j*size + boardLocY, null);
 			}
 		}
+		
+		for(int i = 0; i < availablePieces.size(); i++){
+			//System.out.println(availablePieces.size());
+	//		availablePieces.get(i).render(gd);
+		}
+		
+		currentPiece.render(gd);
+		
 	//	drawSquare(gd, 5, 17);
-		drawJ(gd, 4, 17);
-		drawJ(gd, 9, 17);
+	//	drawJ(gd, 4, 17);
+	//	drawJ(gd, 5, 0);
 	}
 	
 	private void drawSquare(Graphics2D gd, int x, int y) {
@@ -72,33 +96,42 @@ public class GameFrame extends Game{
 		getInput();
 		
 		fallTime += time;
-		if(fallTime >= fallDelay) {
-			fallTime -= fallDelay;
-		}
+		
 		spawn(time);
+		
 	}
 	
 	public void spawn(long time){
 		
 		if(spawn){
-		//	currentPiece
+			nextPieces = availablePieces;
+			Collections.shuffle(nextPieces);
+			currentPiece = nextPieces.get(0);
+			spawn = false;
 		}
+		if(fallTime >= fallDelay) {
+		//	if(!currentPiece.onBorder())
+			currentPiece.moveDown();
+			fallTime -= fallDelay;
+		}
+		
 	}
 	
-
 	private void getInput() {
 		if(keyPressed(KeyEvent.VK_LEFT)) {
-			
-			
+			if(!currentPiece.onBorder())
+			currentPiece.moveLeft();
 		}
 		else if(keyPressed(KeyEvent.VK_RIGHT)) {
-			
+			if(!currentPiece.onBorder())
+			currentPiece.moveRight();
 		}
 		else if(keyPressed(KeyEvent.VK_UP)) {
 			
 		}
 		else if(keyPressed(KeyEvent.VK_DOWN)) {
-			
+			if(!currentPiece.onBorder())
+			currentPiece.moveDown();
 		}
 		
 		if(keyPressed('Z')) {
@@ -112,7 +145,32 @@ public class GameFrame extends Game{
 	
 	// put every type of tetriminos in availablepieces List
 	public void initializePieces(){
+		Tetrimino t;
+		int x = 3, y = 0;
+		String block = "img/J block.png";
+		t = new JPiece(getImage(block), x, y, boardLocX, boardLocY);
+		availablePieces.add(t);
+	//	x = 9; y = 16;
+		block = "img/I block.png";
+		t = new LinePiece(getImage(block), x, y, boardLocX, boardLocY);
+		availablePieces.add(t);
+	//	x = 6; y = 17;
+		block = "img/S block.png";
+		t = new SPiece(getImage(block), x, y, boardLocX, boardLocY);
+		availablePieces.add(t);
+	//	x = 3; y = 17;
+		block = "img/Z block.png";
+		t = new ZPiece(getImage(block), x, y, boardLocX, boardLocY);
+		availablePieces.add(t);	
 		
+/*		block = "img/L block.png";
+		t = new LPiece(getImage(block), x, y, boardLocX, boardLocY);
+		availablePieces.add(t);
+		
+		block = "img/T block.png";
+		t = new TPiece(getImage(block), x, y, boardLocX, boardLocY);
+		availablePieces.add(t);
+*/	
 	}
 
 }
--- src/gamedev/tetris/Tetrimino.java
+++ src/gamedev/tetris/Tetrimino.java
@@ -1,15 +1,76 @@
 package gamedev.tetris;
 
+import java.awt.Graphics2D;
+
 public abstract class Tetrimino {
 	
+	protected final int size = 25;
 	protected final int width = 25;
 	protected final int height = 25;
 	protected Block[][] matrix;
 	
 	abstract public void rotateLeft();
 	abstract public void rotateRight();
-	abstract public void update();
 	abstract public void draw();
+	abstract public void render(Graphics2D g);
+	abstract public String getName();
+	abstract public void setName(String name);
 	
+	public void moveLeft() {
+		int row = matrix.length;
+		int col;
+		for(int i = 0; i < row; i++){
+			col = matrix[i].length;
+			for(int j = 0; j < col; j++){
+				matrix[i][j].setX(matrix[i][j].getX() - 1*size);
+			}
+		}
+	}
 
+	public void moveRight() {
+		int row = matrix.length;
+		int col;
+		for(int i = 0; i < row; i++){
+			col = matrix[i].length;
+			for(int j = 0; j < col; j++){
+				matrix[i][j].setX(matrix[i][j].getX() + 1*size);
+			}
+		}
+	}
+	
+	public void moveDown() {
+		int row = matrix.length;
+		int col;
+		for(int i = 0; i < row; i++){
+			col = matrix[i].length;
+			for(int j = 0; j < col; j++){
+				matrix[i][j].setY(matrix[i][j].getY() + 1*size);
+			}
+		}
+	
+	
+	}
+	
+	
+	public boolean onBorder() {
+		int row = matrix.length;
+		int col; 
+		for(int i = 0; i < row; i++){
+			col = matrix[i].length;
+			for(int j = 0; j < col; j++){
+				if(matrix[i][j].isOccupied()){
+					if(matrix[i][j].getY() > 19 * size){
+						System.out.println("TRUE");
+						return true;
+					}else if(matrix[i][j].getX() < 0 * size || matrix[i][j].getX() > 9 * size){
+						System.out.println("TRUE");
+						
+						return true;
+					}
+				}
+				
+			}
+		}
+		return false;
+	}
 }
