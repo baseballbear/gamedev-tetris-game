12ae505db3d2266872d5b7b7d81752427f06a00c
Jefferson Cordero <jepjephooray05@gmail.com>
2014-10-18 14:12:40 +0800

    fixed a bug on move right/left
    Signed-off-by: Jefferson Cordero <jepjephooray05@gmail.com>

--- src/gamedev/tetris/Tetrimino.java
+++ src/gamedev/tetris/Tetrimino.java
@@ -26,13 +26,26 @@ public abstract class Tetrimino {
 	}
 	
 	public void moveLeft(int boardX) {
+		boolean invalid = false;
 		row = matrix.length;
+		int tempRow, tempCol;
 		
 		for(int i = 0; i < row; i++){
 			col = matrix[i].length;
 			for(int j = 0; j < col; j++){
 				if(matrix[i][j].isOccupied()){
-					if(!(matrix[i][j].getX() < (1 * width) + boardX)){
+					if((matrix[i][j].getX() < (1 * width) + boardX)){
+						invalid = true;
+					}
+				}
+			}
+		}
+		
+		if(!invalid){
+			for(int i = 0; i < row; i++){
+				col = matrix[i].length;
+				for(int j = 0; j < col; j++){
+					if(matrix[i][j].isOccupied()){
 						matrix[i][j].setX(matrix[i][j].getX() - 1*width);
 					}
 				}
@@ -40,17 +53,30 @@ public abstract class Tetrimino {
 		}
 	}
 	public void moveRight(int boardX) {
+		boolean invalid = false;
 		row = matrix.length;
 		
 		for(int i = 0; i < row; i++){
 			col = matrix[i].length;
 			for(int j = 0; j < col; j++){
 				if(matrix[i][j].isOccupied()){
-					if(!(matrix[i][j].getX() > (8 * width) + boardX)){
+					if((matrix[i][j].getX() > (8 * width) + boardX)){
+						invalid = true;
+					}
+				}
+			}
+		}
+		
+		if(!invalid){
+			for(int i = 0; i < row; i++){
+				col = matrix[i].length;
+				for(int j = 0; j < col; j++){
+					if(matrix[i][j].isOccupied()){
 						matrix[i][j].setX(matrix[i][j].getX() + 1*width);
 					}
 				}
 			}
+			
 		}
 	}
 	
