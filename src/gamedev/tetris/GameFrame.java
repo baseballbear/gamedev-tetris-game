9346a0e675356d7f2cb6b1ed6f54b07967142b71
jeremiah-luy <jeremiahluy@gmail.com>
2014-10-21 23:16:18 +0800

    Added some menu screens
    Added the main menu and pause screens.
    Press 'escape' while in-game to go to the pause screen.
    The pause screen is currently controlled by keyboard:
    'R'  for restart
    'Q' for exit to main menu

--- src/gamedev/tetris/GameFrame.java
+++ src/gamedev/tetris/GameFrame.java
@@ -24,7 +24,7 @@ public class GameFrame extends Game{
 			size = 25, boardLocX, boardLocY,
 			numOfPieces = 6, timer = 0, speed = 1, saveCount = 0;
 	private Block[][] board;
-	
+
 	List<Tetrimino> savedPieces, // the pieces that were saved by the user
 	nextPieces,  // the pieces that are next on the list
 	nextPieces2,
@@ -35,14 +35,25 @@ public class GameFrame extends Game{
 	public enum Direction{
 		Left, Down, Right;
 	}
- 	
-	long fallTime, fallDelay = 500;
+
+	public enum Screen {
+		GAME_SCREEN,
+		MAIN_MENU,
+		GAME_OVER,
+		PAUSE_SCREEN
+	}
+	Screen currentScreen;
+	List<Button> menuButtons;
 	
+	long fallTime, fallDelay = 500;
+
 	//empty constructor
 	public GameFrame(){};
 
 	@Override
 	public void initResources() {
+
+		//Initialize Game Components
 		board = new Block[width][height];
 		nextPieces = new ArrayList<Tetrimino>();
 		nextPieces2 = new ArrayList<Tetrimino>();
@@ -52,74 +63,106 @@ public class GameFrame extends Game{
 		initializeBoard();
 		initGameState();
 		initializePieces();
+
+		currentScreen = Screen.MAIN_MENU;
+		menuButtons = new ArrayList<Button>();
+		menuButtons.add(new Button(getImage("img/buttons/play2.png"), 0, 0, "Start"));
+		menuButtons.add(new Button(getImage("img/buttons/highscores.png"), 0, 70, "Highscores"));
+		menuButtons.add(new Button(getImage("img/buttons/help.png"), 0, 140, "Help"));
+		menuButtons.add(new Button(getImage("img/buttons/settings.png"), 0, 210, "Settings"));
 	}
-	
+
 	private void initGameState() {
 		saveCount = 0;
 		fallTime = 0;
 		fallDelay = 900;
-		
+
 	}
-	
+
 	public void initializeBoard(){
 		String image = "img/empty.png";
-		
+
 		for(int i = 0; i < width; i++){
 			for(int j = 0; j < height; j++){
 				board[i][j] = new Block(getImage(image), i*size + boardLocX, j*size + boardLocY);
 			}
 		}
 	}
-	
+
 	@Override
 	public void render(Graphics2D gd) {
+		switch(currentScreen) {
+			case GAME_SCREEN:
+				gd.setColor(Color.black);
+				gd.fillRect(0, 0, getWidth(), getHeight());
 		
-		gd.setColor(Color.black);
-		gd.fillRect(0, 0, getWidth(), getHeight());
+				gd.setColor(Color.white);
+				gd.drawImage(getImage("img/board.png"), boardLocX - 126, boardLocY - 15, null);
 		
-		gd.setColor(Color.white);
-		gd.drawImage(getImage("img/board.png"), boardLocX - 126, boardLocY - 15, null);
-
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				board[i][j].render(gd);
-			}
+				for (int i = 0; i < width; i++) {
+					for (int j = 0; j < height; j++) {
+						board[i][j].render(gd);
+					}
+				}
+		
+				if(!nextPieces.isEmpty())
+					displayNext(gd);
+				if(!savedPieces.isEmpty())
+					displaySaved(gd);
+				if(isTopOccupied()){
+					gameOver(gd);
+				}
+				else if(currentPiece != null)
+					currentPiece.render(gd);
+				break;
+			case MAIN_MENU:
+				gd.setColor(Color.black);
+				gd.fillRect(0, 0, getWidth(), getHeight());
+				
+				for(Button b : menuButtons)
+					b.render(gd);
+				break;
+			case GAME_OVER:
+				break;
+			default:
+				break;
 		}
-		
-		if(!nextPieces.isEmpty())
-			displayNext(gd);
-		if(!savedPieces.isEmpty())
-			displaySaved(gd);
-		if(isTopOccupied()){
-			gameOver(gd);
-		}
-		else
-		currentPiece.render(gd);
 	}
-	
+
 	@Override
 	public void update(long time) {
-		// TODO Auto-generated method stub
+		switch(currentScreen) {
+			case GAME_SCREEN:
+				fallTime += time;
 		
-		fallTime += time;
+				if(nextPieces2.isEmpty()){
+					setUpNextPieces("nextPieces2");
+				}
+				if(nextPieces.isEmpty()){
+					setUpNextPieces("nextPieces");
+					spawn(time);
+				}
+				else{
+					spawn(time);
+				}
+				getInput();
 		
-		if(nextPieces2.isEmpty()){
-			setUpNextPieces("nextPieces2");
+				break;
+			case MAIN_MENU:
+				getMainMenuInput();
+				break;
+			case GAME_OVER:
+				break;
+			case PAUSE_SCREEN:
+				getPauseMenuInput();
+				break;
+			default:
+				break;
 		}
-		if(nextPieces.isEmpty()){
-			setUpNextPieces("nextPieces");
-			spawn(time);
-		}
-		else{
-			spawn(time);
-		}
-		getInput();
-
 	}
-	
 	private void checkLine() {
 		boolean lineComplete = true;
-		
+
 		for(int i = 0; i < height; i++) {
 			for(int j = 0; j < width; j++) {
 				if(!board[j][i].isOccupied())
@@ -133,21 +176,21 @@ public class GameFrame extends Game{
 					}
 				}
 				String image = "img/empty.png";
-				
+
 				for(int m = 0; m < width; m++) {
 					board[m][0].setOccupied(false);
 					board[m][0].setImage(getImage(image));
 				}
 			}
 			lineComplete = true;
-			
+
 		}
-		
+
 	}
 
 	public void setUpNextPieces(String listName){
 		initializePieces();
-		
+
 		for(int i = 0; i < availablePieces.size(); i++){
 			if(listName.equals("nextPieces")){
 				nextPieces.add(availablePieces.get(i));
@@ -158,13 +201,13 @@ public class GameFrame extends Game{
 				Collections.shuffle(nextPieces2);
 			}
 		}
-		
+
 	}
-	
+
 	public void spawn(long time){
-		
+
 		checkLine();
-		
+
 		if(spawn){
 			currentPiece = nextPieces.get(0);
 			nextPieces.remove(0);
@@ -173,7 +216,7 @@ public class GameFrame extends Game{
 				nextPieces2.remove(0);
 			}
 			spawn = false;
-			
+
 		}
 		if(fallTime >= fallDelay) {
 			if(!checkCollision(GameFrame.Direction.Down)){
@@ -188,34 +231,34 @@ public class GameFrame extends Game{
 			}
 			fallTime -= fallDelay;
 		}
-		
+
 	}
-	
+
 	public boolean isTopOccupied(){
-		
+
 		for(int i = 0; i < board.length; i++){
 			for(int j = 0; j < 2; j++){
 				if(board[i][j].isOccupied()){
 					return true;
 				}
-				
+
 			}
 		}
 		return false;
 	}
-	
+
 	//temporary lang to 
 	private void gameOver(Graphics2D gd){
 		spawn = false;
 		Font big = new Font("Berlin Sans FB Demi", Font.BOLD, 60);
-		
+
 		gd.setColor(Color.black);
 		gd.fillRect(0, 0, getWidth(), getHeight());
 		gd.setColor(Color.yellow);
 		gd.setFont(big);
 		gd.drawString("Game Over", getWidth()/2 - 150, getHeight()/2);
 	}
-	
+
 	private void getInput() {
 		if(keyPressed(KeyEvent.VK_LEFT)) {
 			if(!checkCollision(Direction.Left)){
@@ -227,7 +270,7 @@ public class GameFrame extends Game{
 				currentPiece.moveRight(boardLocX);
 		}
 		else if(keyPressed(KeyEvent.VK_UP)) {
-			
+
 		}
 		else if(keyDown(KeyEvent.VK_DOWN)) {
 			if(!checkCollision(Direction.Down))
@@ -239,29 +282,31 @@ public class GameFrame extends Game{
 		}else if(keyPressed(KeyEvent.VK_R)) {
 			//	initResources();
 		}else if(keyPressed(KeyEvent.VK_SHIFT) || keyPressed(KeyEvent.VK_C)){
-				if(savedPieces.size() < 3){
-					savedPieces.add(currentPiece);
-					spawn = true;
-					saveCount++;
-				}
-				else{
+			if(savedPieces.size() < 3){
+				savedPieces.add(currentPiece);
+				spawn = true;
+				saveCount++;
+			}
+			else{
 				//	if(saveCount < 2){
-						Tetrimino piece = savedPieces.get(0);
-						savedPieces.remove(0);
-						savedPieces.add(currentPiece);
-						piece.setLocation(3, 0);
-						currentPiece = piece;
+				Tetrimino piece = savedPieces.get(0);
+				savedPieces.remove(0);
+				savedPieces.add(currentPiece);
+				piece.setLocation(3, 0);
+				currentPiece = piece;
 				//	}
-				}
-			
+			}
+
+		} else if(keyPressed(KeyEvent.VK_ESCAPE)) {
+			currentScreen = Screen.PAUSE_SCREEN;
 		}
-		
-		
+
+
 		if(keyPressed('Z')) {
-			
+
 		}
 		else if(keyPressed('X')) {
-			
+
 		}
 	}
 	private void displayNext(Graphics2D gd){
@@ -272,7 +317,7 @@ public class GameFrame extends Game{
 			gd.drawImage(getImage("img/smallpieces/" + nextPieces.get(i).getImageName() + ".png"), locx, locy, null);
 		}
 	}
-	
+
 	private void displaySaved(Graphics2D gd){
 		int locx = boardLocX - 100;
 		int locy = 0;
@@ -281,7 +326,7 @@ public class GameFrame extends Game{
 			gd.drawImage(getImage("img/smallpieces/" + savedPieces.get(i).getImageName() + ".png"), locx, locy, null);
 		}
 	}
-	
+
 	public boolean checkCollision(Direction d){
 		int row = currentPiece.matrix.length;
 		int col = currentPiece.matrix[0].length;
@@ -298,7 +343,7 @@ public class GameFrame extends Game{
 				}
 			}
 			return false;
-			
+
 		case Down:
 			if(currentPiece.getY() + currentPiece.getRow() >= height)
 				return true;
@@ -321,12 +366,12 @@ public class GameFrame extends Game{
 				}
 			}
 			return false;
-			
+
 		}
-		
+
 		return false;
 	}
-	
+
 	// put every type of tetriminos in availablepieces List
 	public void initializePieces(){
 		availablePieces = new ArrayList<Tetrimino>();
@@ -335,32 +380,55 @@ public class GameFrame extends Game{
 		String block = "img/I block.png";
 		t = new LinePiece(getImage(block), x, y, boardLocX, boardLocY);
 		availablePieces.add(t);
-		
+
 		x = 3; y = -1;
 		block = "img/J block.png";
 		t = new JPiece(getImage(block), x, y, boardLocX, boardLocY);
 		availablePieces.add(t);
-		
+
 		block = "img/S block.png";
 		t = new SPiece(getImage(block), x, y, boardLocX, boardLocY);
 		availablePieces.add(t);
-		
+
 		block = "img/Z block.png";
 		t = new ZPiece(getImage(block), x, y, boardLocX, boardLocY);
 		availablePieces.add(t);	
-		
+
 		block = "img/L block.png";
 		t = new LPiece(getImage(block), x, y, boardLocX, boardLocY);
 		availablePieces.add(t);
-		
+
 		block = "img/T block.png";
 		t = new TPiece(getImage(block), x, y, boardLocX, boardLocY);
 		availablePieces.add(t);
-		
+
 		block = "img/square Block.png";
 		t = new SquarePiece(getImage(block), x + 1, y + 1, boardLocX, boardLocY);
 		availablePieces.add(t);
-		
+
+	}
+
+	private void getPauseMenuInput() {
+		if(keyPressed(KeyEvent.VK_R)) {
+			initializeBoard();
+			initGameState();
+			initializePieces();
+			currentScreen = Screen.GAME_SCREEN;
+		} else if(keyPressed(KeyEvent.VK_Q)) {
+			currentScreen = Screen.MAIN_MENU;
+		}
+			
+	}
+
+	private void getMainMenuInput() {
+		if(click()) {
+			int x = getMouseX(), y = getMouseY();
+			for(Button b : menuButtons)
+				if(b.contains(x, y))
+					if(b.getBtnName().equals("Start")) {
+						currentScreen = Screen.GAME_SCREEN;
+					}
+		}
 	}
 
 }
