b2f8f943b5075f22657413a01c32f5ba3bebd058
baseballbear <rbecruz@gmail.com>
2014-10-26 19:05:12 +0800

    Extreme Pieces

---
+++ src/gamedev/piece/RectanglePiece.java
@@ -0,0 +1,27 @@
+package gamedev.piece;
+
+import gamedev.tetris.Tetrimino;
+
+import java.awt.image.BufferedImage;
+
+public class RectanglePiece extends Tetrimino{
+	
+	public RectanglePiece(BufferedImage image, int x, int y, int boardX, int boardY){
+		super(x,y);
+		setCol(3);
+		setRow(3);
+		this.boardX = boardX;
+		this.boardY = boardY;
+		initializeMatrix(image);
+		super.imageName = "small_i3";
+	
+		for(int i = 0; i < 2; i++){
+			for(int j = 0; j < 3; j++){
+				matrix[j][i].setOccupied(true);
+			}
+		}
+			
+	}
+	
+	
+}
